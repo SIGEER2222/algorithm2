7fc93691651a6ad3bf203170007229f6d1b06aab
SIGEER2222 <2244828416@qq.com>
2022-04-05 20:25:02 +0800

    1

---
+++ Statics/Class1.cs
@@ -0,0 +1,11 @@
+﻿global using System;
+global using System.Linq;
+
+namespace Statics
+{
+    public static class Class1
+    {
+        public static void OUT(this object s) => Console.WriteLine(s);
+
+    }
+}
